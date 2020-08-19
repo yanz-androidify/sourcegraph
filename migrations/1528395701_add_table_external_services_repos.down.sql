bea1e4b70c0346129d063e05cf47ba3e55e3382f
Asdine El Hrychy <asdine.elhrychy@gmail.com>
2020-08-19 16:56:16 +0200

    Add missing parentheses

--- migrations/1528395701_add_table_external_services_repos.down.sql
+++ migrations/1528395701_add_table_external_services_repos.down.sql
@@ -3,8 +3,8 @@ BEGIN;
 DROP TRIGGER IF EXISTS trig_soft_delete_repo_reference_on_external_service_repos ON repo;
 DROP TRIGGER IF EXISTS trig_soft_delete_external_service_reference_on_external_service_repos ON repo;
 
-DROP FUNCTION IF EXISTS soft_delete_external_service_reference_on_external_service_repos;
-DROP FUNCTION IF EXISTS soft_delete_repo_reference_on_external_service_repos;
+DROP FUNCTION IF EXISTS soft_delete_external_service_reference_on_external_service_repos();
+DROP FUNCTION IF EXISTS soft_delete_repo_reference_on_external_service_repos();
 
 DROP TABLE IF EXISTS external_service_repos;
 ALTER TABLE repo ADD COLUMN sources jsonb DEFAULT '{}'::jsonb NOT NULL;
